b2b543d6cea38e8577eb43c4c82389f911af1810
yuyixg <yuyixg@gmail.com>
2020-07-09 10:03:19 +0800

    Initialize netcore_31_migration

---
+++ src/Application/Duber.Invoice.API/Startup.cs
@@ -0,0 +1,275 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using AutoMapper;
+using Duber.Domain.ACL.Adapters;
+using Duber.Domain.ACL.Contracts;
+using Duber.Domain.Invoice.Persistence;
+using Duber.Domain.Invoice.Repository;
+using Duber.Domain.Invoice.Services;
+using Duber.Infrastructure.EventBus;
+using Duber.Infrastructure.EventBus.Abstractions;
+using Duber.Infrastructure.EventBus.RabbitMQ;
+using Duber.Infrastructure.EventBus.ServiceBus;
+using Duber.Infrastructure.Resilience;
+using Duber.Infrastructure.Resilience.Http;
+using Duber.Infrastructure.Resilience.SqlServer;
+using Duber.Invoice.API.Application.IntegrationEvents.Events;
+using Duber.Invoice.API.Application.IntegrationEvents.Hnadlers;
+using Duber.Invoice.API.Application.Validations;
+using Duber.Invoice.API.Infrastructure.AutofacModules;
+using Duber.Invoice.API.Infrastructure.Filters;
+using FluentValidation.AspNetCore;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+// ReSharper disable InconsistentNaming
+// ReSharper disable AssignNullToNotNullAttribute
+#pragma warning disable 618
+
+namespace Duber.Invoice.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+            services.AddAutoMapper();
+            services.AddMvc(options =>
+                {
+                    options.Filters.Add(typeof(HttpGlobalExceptionFilter));
+                    options.Filters.Add(typeof(ValidatorActionFilter));
+                })
+                .AddFluentValidation(x => x.RegisterValidatorsFromAssemblyContaining<CreateInvoiceRequestValidator>());
+
+            services.AddOptions();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+            });
+
+            // just to perform the migrations
+            services.AddDbContext<InvoiceMigrationContext>(options =>
+            {
+                options.UseSqlServer(
+                    Configuration["ConnectionString"],
+                    sqlOptions =>
+                    {
+                        sqlOptions.MigrationsAssembly(typeof(InvoiceMigrationContext).GetTypeInfo().Assembly.GetName().Name);
+                        sqlOptions.EnableRetryOnFailure(maxRetryCount: 5, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+                    });
+            });
+
+            // swagger configuration
+            services.AddSwaggerGen(options =>
+            {
+                options.DescribeAllEnumsAsStrings();
+                options.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info
+                {
+                    Title = "Duber.Invoice HTTP API",
+                    Version = "v1",
+                    Description = "The Duber Invoice Service HTTP API",
+                    TermsOfService = "Terms Of Service"
+                });
+
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetEntryAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                options.IncludeXmlComments(xmlPath);
+            });
+
+            // Resilient SQL Executor configuration.
+            services.AddSingleton(sp =>
+            {
+                var logger = sp.GetRequiredService<ILogger<ResilientExecutor<ISqlExecutor>>>();
+
+                var retryCount = 5;
+                if (!string.IsNullOrEmpty(Configuration["SqlClientRetryCount"]))
+                {
+                    retryCount = int.Parse(Configuration["SqlClientRetryCount"]);
+                }
+
+                var exceptionsAllowedBeforeBreaking = 4;
+                if (!string.IsNullOrEmpty(Configuration["SqlClientExceptionsAllowedBeforeBreaking"]))
+                {
+                    exceptionsAllowedBeforeBreaking = int.Parse(Configuration["SqlClientExceptionsAllowedBeforeBreaking"]);
+                }
+
+                return new ResilientSqlExecutorFactory(logger, exceptionsAllowedBeforeBreaking, retryCount);
+            });
+            services.AddTransient(sp => sp.GetService<ResilientSqlExecutorFactory>().CreateResilientSqlClient());
+
+            // Invoice repository and context configuration
+            services.AddTransient<IInvoiceContext, InvoiceContext>(provider =>
+            {
+                var mediator = provider.GetService<IMediator>();
+                var sqlExecutor = provider.GetService<ResilientExecutor<ISqlExecutor>>();
+                var connectionString = Configuration["ConnectionString"];
+                return new InvoiceContext(connectionString, mediator, sqlExecutor);
+            });
+            services.AddTransient<IInvoiceRepository, InvoiceRepository>();
+
+            // payment service configuration
+            services.AddTransient<IPaymentService, PaymentService>();
+            services.AddTransient<IPaymentServiceAdapter, PaymentServiceAdapter>(provider =>
+            {
+                var httpInvoker = provider.GetRequiredService<ResilientHttpInvoker>();
+                var paymentServiceBaseUrl = Configuration["PaymentServiceBaseUrl"];
+                return new PaymentServiceAdapter(httpInvoker, paymentServiceBaseUrl);
+            });
+
+            // Resilient Http Invoker onfiguration.
+            services.AddSingleton(sp =>
+            {
+                var logger = sp.GetRequiredService<ILogger<ResilientHttpInvoker>>();
+
+                var retryCount = 5;
+                if (!string.IsNullOrEmpty(Configuration["HttpClientRetryCount"]))
+                {
+                    retryCount = int.Parse(Configuration["HttpClientRetryCount"]);
+                }
+
+                var exceptionsAllowedBeforeBreaking = 4;
+                if (!string.IsNullOrEmpty(Configuration["HttpClientExceptionsAllowedBeforeBreaking"]))
+                {
+                    exceptionsAllowedBeforeBreaking = int.Parse(Configuration["HttpClientExceptionsAllowedBeforeBreaking"]);
+                }
+
+                return new ResilientHttpInvokerFactory(logger, exceptionsAllowedBeforeBreaking, retryCount);
+            });
+            services.AddTransient(sp => sp.GetService<ResilientHttpInvokerFactory>().CreateResilientHttpClient());
+
+            // service bus configuration
+            if (Configuration.GetValue<bool>("AzureServiceBusEnabled"))
+            {
+                services.AddSingleton<IServiceBusPersisterConnection>(sp =>
+                {
+                    var logger = sp.GetRequiredService<ILogger<DefaultServiceBusPersisterConnection>>();
+                    var serviceBusConnectionString = Configuration["EventBusConnection"];
+                    var serviceBusConnection = new ServiceBusConnectionStringBuilder(serviceBusConnectionString);
+
+                    return new DefaultServiceBusPersisterConnection(serviceBusConnection, logger);
+                });
+            }
+            else
+            {
+                services.AddSingleton<IRabbitMQPersistentConnection>(sp =>
+                {
+                    var logger = sp.GetRequiredService<ILogger<DefaultRabbitMQPersistentConnection>>();
+                    var factory = new ConnectionFactory()
+                    {
+                        HostName = Configuration["EventBusConnection"]
+                    };
+
+                    var retryCount = 5;
+                    if (!string.IsNullOrEmpty(Configuration["EventBusRetryCount"]))
+                    {
+                        retryCount = int.Parse(Configuration["EventBusRetryCount"]);
+                    }
+
+                    return new DefaultRabbitMQPersistentConnection(factory, logger, retryCount);
+                });
+            }
+                        
+            RegisterEventBus(services);
+
+            //configure autofac
+            var container = new ContainerBuilder();
+            container.Populate(services);
+            container.RegisterModule(new MediatorModule());
+
+            return new AutofacServiceProvider(container.Build());
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddAzureWebAppDiagnostics();
+            loggerFactory.AddApplicationInsights(app.ApplicationServices, LogLevel.Trace);
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseCors("CorsPolicy");
+            app.UseMvc();
+            ConfigureEventBus(app);
+
+            app.UseSwagger()
+                .UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Duber.Invoice V1");
+                    c.RoutePrefix = string.Empty;
+                });
+        }
+
+        private void RegisterEventBus(IServiceCollection services)
+        {
+            var retryCount = 5;
+            if (!string.IsNullOrEmpty(Configuration["EventBusRetryCount"]))
+            {
+                retryCount = int.Parse(Configuration["EventBusRetryCount"]);
+            }
+
+            if (Configuration.GetValue<bool>("AzureServiceBusEnabled"))
+            {
+                services.AddSingleton<IEventBus, EventBusServiceBus>(sp =>
+                {
+                    var serviceBusPersisterConnection = sp.GetRequiredService<IServiceBusPersisterConnection>();
+                    var iLifetimeScope = sp.GetRequiredService<ILifetimeScope>();
+                    var logger = sp.GetRequiredService<ILogger<EventBusServiceBus>>();
+                    var eventBusSubcriptionsManager = sp.GetRequiredService<IEventBusSubscriptionsManager>();
+                    var subscriptionClientName = Configuration["SubscriptionClientName"];
+
+                    return new EventBusServiceBus(serviceBusPersisterConnection, logger,
+                        eventBusSubcriptionsManager, subscriptionClientName, iLifetimeScope, retryCount);
+                });
+            }
+            else
+            {
+                services.AddSingleton<IEventBus, EventBusRabbitMQ>(sp =>
+                {
+                    var rabbitMQPersistentConnection = sp.GetRequiredService<IRabbitMQPersistentConnection>();
+                    var iLifetimeScope = sp.GetRequiredService<ILifetimeScope>();
+                    var logger = sp.GetRequiredService<ILogger<EventBusRabbitMQ>>();
+                    var eventBusSubcriptionsManager = sp.GetRequiredService<IEventBusSubscriptionsManager>();
+
+                    return new EventBusRabbitMQ(rabbitMQPersistentConnection, logger, iLifetimeScope, eventBusSubcriptionsManager, retryCount);
+                });
+            }
+            
+            services.AddSingleton<IEventBusSubscriptionsManager, InMemoryEventBusSubscriptionsManager>();
+            services.AddTransient<TripCancelledIntegrationEventHandler>();
+            services.AddTransient<TripFinishedIntegrationEventHandler>();
+        }
+
+        private void ConfigureEventBus(IApplicationBuilder app)
+        {
+            var eventBus = app.ApplicationServices.GetRequiredService<IEventBus>();
+            eventBus.Subscribe<TripCancelledIntegrationEvent, TripCancelledIntegrationEventHandler>();
+            eventBus.Subscribe<TripFinishedIntegrationEvent, TripFinishedIntegrationEventHandler>();
+        }
+    }
+}
---
+++ src/Web/Duber.WebSite/Controllers/TripController.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Duber.Domain.Driver.Model;
+using Duber.Domain.Driver.Repository;
+using Duber.Domain.User.Model;
+using Duber.Domain.User.Repository;
+using Duber.Infrastructure.Resilience.Http;
+using Duber.WebSite.Extensions;
+using Duber.WebSite.Hubs;
+using Duber.WebSite.Infrastructure.Repository;
+using Duber.WebSite.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using RestSharp;
+// ReSharper disable ForCanBeConvertedToForeach
+
+namespace Duber.WebSite.Controllers
+{
+    public class TripController : Controller
+    {
+        private readonly IMemoryCache _cache;
+        private readonly IUserRepository _userRepository;
+        private readonly ResilientHttpInvoker _httpInvoker;
+        private readonly IHubContext<TripHub> _hubContext;
+        private readonly IDriverRepository _driverRepository;
+        private readonly IReportingRepository _reportingRepository;
+        private readonly IOptions<TripApiSettings> _tripApiSettings;
+        private readonly Dictionary<SelectListItem, LocationModel> _originsAndDestinations;
+
+        public TripController(IUserRepository userRepository,
+            IDriverRepository driverRepository,
+            IMemoryCache cache,
+            ResilientHttpInvoker httpInvoker,
+            IOptions<TripApiSettings> tripApiSettings,
+            IHubContext<TripHub> hubContext,
+            IReportingRepository reportingRepository)
+        {
+            _userRepository = userRepository;
+            _driverRepository = driverRepository;
+            _cache = cache;
+            _httpInvoker = httpInvoker;
+            _tripApiSettings = tripApiSettings;
+            _hubContext = hubContext;
+            _reportingRepository = reportingRepository;
+
+            _originsAndDestinations = new Dictionary<SelectListItem, LocationModel>
+            {
+                {
+                    new SelectListItem { Text = "Poblado's Park" },
+                    new LocationModel { Latitude = 6.210292869847029, Longitude = -75.57115852832794, Description = "Poblado's Park" }
+                },
+                {
+                    new SelectListItem { Text = "Lleras Park" },
+                    new LocationModel { Latitude = 6.2087793817882515, Longitude = -75.56776275426228, Description = "Lleras Park" }
+                },
+                {
+                    new SelectListItem { Text = "Sabaneta Park" },
+                    new LocationModel { Latitude = 6.151584634798451, Longitude = -75.61546325683594, Description = "Sabaneta Park" }
+                },
+                {
+                    new SelectListItem { Text = "The executive bar" },
+                    new LocationModel { Latitude = 6.252063572976704, Longitude = -75.56599313040351, Description = "The executive bar" }
+                },
+            };
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var drivers = await GetDrivers();
+            var users = await GetUsers();
+
+            var model = new TripRequestModel
+            {
+                Drivers = drivers.ToSelectList(),
+                Users = users.ToSelectList(),
+                User = users.FirstOrDefault()?.Id.ToString(),
+                Driver = drivers.FirstOrDefault()?.Id.ToString(),
+                Origins = _originsAndDestinations.Keys.Select(x => x).ToList(),
+                Destinations = _originsAndDestinations.Keys.Select(x => x).ToList(),
+                From = _originsAndDestinations.Keys.Select(x => x).ToList().FirstOrDefault()?.Text,
+                To = _originsAndDestinations.Keys.Select(x => x).ToList().LastOrDefault()?.Text,
+                Places = _originsAndDestinations.Values.Select(x => x).ToList()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SimulateTrip(TripRequestModel model)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState.AllErrors());
+
+            var createdTask = CreateTrip(model);
+            var createdNotificationTask = _hubContext.Clients.All.SendAsync("NotifyTrip", "Created");
+            await Task.WhenAll(createdTask, createdNotificationTask);
+            var tripID = await createdTask;
+
+            var acceptedTask = AcceptOrStartTrip(_tripApiSettings.Value.AcceptUrl, tripID);
+            var acceptedNotificationTask = _hubContext.Clients.All.SendAsync("NotifyTrip", "Accepted");
+            await Task.WhenAll(acceptedTask, acceptedNotificationTask);
+
+            var startedTask = AcceptOrStartTrip(_tripApiSettings.Value.StartUrl, tripID);
+            var startedNotificationTask = _hubContext.Clients.All.SendAsync("NotifyTrip", "Started");
+            await Task.WhenAll(startedTask, startedNotificationTask);
+
+            var updatedPositionTasks = new List<Task>();
+            for (var index = 0; index < model.Directions.Count; index += 5)
+            {
+                var direction = model.Directions[index];
+                if (index + 5 >= model.Directions.Count)
+                    direction = _originsAndDestinations.Values.SingleOrDefault(x => x.Description == model.To);
+
+                updatedPositionTasks.Add(UpdateTripLocation(tripID, direction));
+                updatedPositionTasks.Add(_hubContext.Clients.All.SendAsync("UpdateCurrentPosition", direction));
+            }
+
+            updatedPositionTasks.Add(_hubContext.Clients.All.SendAsync("NotifyTrip", "Finished"));
+            await Task.WhenAll(updatedPositionTasks);
+
+            return Ok();
+        }
+
+        public async Task<IActionResult> TripsByUser()
+        {
+            var users = await GetUsers();
+            var usersModel = users.Select(x => new UserModel
+            {
+                Id = x.Id,
+                Email = x.Email,
+                Name = x.Name,
+                NumberPhone = x.NumberPhone
+            });
+
+            return View(usersModel.ToList());
+        }
+
+        public async Task<IActionResult> TripsByUserId(int id)
+        {
+            try
+            {
+                var trips = await _reportingRepository.GetTripsByUserAsync(id);
+                return View("UserTrips", trips.ToList());
+            }
+            finally
+            {
+                _reportingRepository.Dispose();
+            }
+        }
+
+        public async Task<IActionResult> TripById(Guid id)
+        {
+            try
+            {
+                var trip = await _reportingRepository.GetTripAsync(id);
+                return View("TripDetails", trip);
+            }
+            finally
+            {
+                _reportingRepository.Dispose();
+            }
+        }
+
+        public async Task<IActionResult> TripsByDriver()
+        {
+            var drivers = await GetDrivers();
+            var driversModel = drivers.Select(x => new DriverModel()
+            {
+                Id = x.Id,
+                Email = x.Email,
+                Name = x.Name,
+                NumberPhone = x.PhoneNumber
+            });
+
+            return View(driversModel.ToList());
+        }
+
+        public async Task<IActionResult> TripsByDriverId(int id)
+        {
+            try
+            {
+                var trips = await _reportingRepository.GetTripsByDriverAsync(id);
+                return View("DriverTrips", trips.ToList());
+            }
+            finally
+            {
+                _reportingRepository.Dispose();
+            }
+        }
+
+        public async Task<IActionResult> Test()
+        {
+            var model = new TripRequestModel
+            {
+                Driver = (await GetDrivers()).FirstOrDefault()?.Id.ToString(),
+                User = (await GetUsers()).FirstOrDefault()?.Id.ToString(),
+                From = "Poblado's Park",
+                To = "Sabaneta Park"
+            };
+
+            await CreateTrip(model);
+            return Ok();
+        }
+
+        private async Task<IList<Driver>> GetDrivers()
+        {
+            var drivers = await GetDataFromCache("drivers", () => _driverRepository.GetDriversAsync());
+            return drivers;
+        }
+
+        private async Task<IList<User>> GetUsers()
+        {
+            var users = await GetDataFromCache("users", () => _userRepository.GetUsersAsync());
+            return users;
+        }
+
+        private async Task<T> GetDataFromCache<T>(string cacheKey, Func<Task<T>> action)
+        {
+            if (!_cache.TryGetValue(cacheKey, out T result))
+            {
+                result = await action();
+
+                var cacheEntryOptions = new MemoryCacheEntryOptions()
+                    .SetSlidingExpiration(TimeSpan.FromMinutes(5));
+
+                _cache.Set(cacheKey, result, cacheEntryOptions);
+            }
+
+            return result;
+        }
+
+        private async Task<Guid> CreateTrip(TripRequestModel model)
+        {
+            var drivers = await GetDrivers();
+            var users = await GetUsers();
+            var driverInfo = drivers.SingleOrDefault(x => x.Id == int.Parse(model.Driver));
+            var userInfo = users.SingleOrDefault(x => x.Id == int.Parse(model.User));
+
+            var createRequestRresponse = await _httpInvoker.InvokeAsync(async () =>
+            {
+                var client = new RestClient(_tripApiSettings.Value.BaseUrl);
+                var request = new RestRequest(_tripApiSettings.Value.CreateUrl, Method.POST);
+                request.AddJsonBody(new
+                {
+                    userId = int.Parse(model.User),
+                    driverId = int.Parse(model.Driver),
+                    from = _originsAndDestinations.Values.SingleOrDefault(x => x.Description == model.From),
+                    to = _originsAndDestinations.Values.SingleOrDefault(x => x.Description == model.To),
+                    plate = driverInfo?.CurrentVehicle.Plate,
+                    brand = driverInfo?.CurrentVehicle.Brand,
+                    model = driverInfo?.CurrentVehicle.Model,
+                    paymentMethod = userInfo?.PaymentMethod
+                });
+
+                return await client.ExecuteTaskAsync(request);
+            });
+
+            if (createRequestRresponse.StatusCode != HttpStatusCode.Created)
+                throw new InvalidOperationException("There was an error with Trip service", createRequestRresponse.ErrorException);
+
+            return JsonConvert.DeserializeObject<Guid>(createRequestRresponse.Content);
+        }
+
+        private async Task AcceptOrStartTrip(string action, Guid tripId)
+        {
+            var createRequestRresponse = await _httpInvoker.InvokeAsync(async () =>
+            {
+                var client = new RestClient(_tripApiSettings.Value.BaseUrl);
+                var request = new RestRequest(action, Method.PUT);
+                request.AddJsonBody(new { id = tripId.ToString() });
+
+                return await client.ExecuteTaskAsync(request);
+            });
+
+            if (createRequestRresponse.StatusCode != HttpStatusCode.OK)
+                throw new InvalidOperationException("There was an error with Trip service", createRequestRresponse.ErrorException);
+        }
+
+        private async Task UpdateTripLocation(Guid tripId, LocationModel location)
+        {
+            var createRequestRresponse = await _httpInvoker.InvokeAsync(async () =>
+            {
+                var client = new RestClient(_tripApiSettings.Value.BaseUrl);
+                var request = new RestRequest(_tripApiSettings.Value.UpdateCurrentLocationUrl, Method.PUT);
+                request.AddJsonBody(new
+                {
+                    id = tripId,
+                    currentLocation = new { latitude = location.Latitude, longitude = location.Longitude }
+                });
+
+                return await client.ExecuteTaskAsync(request);
+            });
+
+            if (createRequestRresponse.StatusCode != HttpStatusCode.OK)
+                throw new InvalidOperationException("There was an error with Trip service", createRequestRresponse.ErrorException);
+        }
+    }
+}
